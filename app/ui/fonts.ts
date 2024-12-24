87ab613bb35decd1d07ba710a67a3df7ffbccddc
quanglbm2 <quanglbm2@vng.com.vn>
2024-12-24 10:25:12 +0700

    make app

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana, Lustria } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin', 'vietnamese'] });
+export const lusitana = Lustria({
+    subsets: ['latin'],
+    weight: ['400']
+})
